89425e07c5bad677edac60e222fdd11fc42106fe
Devin Rasmussen <devin.rasmussen@icloud.com>
2021-07-16 06:09:40 +0700

    Introduce tests for progress bar, clean up naming of variables

--- src/progress_bar_exercise/ProgressBar/progress-bar.component.js
+++ src/progress_bar_exercise/ProgressBar/progress-bar.component.js
@@ -4,28 +4,38 @@ import React, { useEffect, useState } from "react";
 export default function ProgressBar({ loading }) {
   const [seconds, setSeconds] = useState(0)
   const [opacity, setOpacity] = useState(1)
+  const [visible, setVisible] = useState()
 
   useEffect(() => {
-    if (seconds && !loading) { // loading was toggled to false and we have a count going
+    if (seconds && !loading) { // loading was toggled to false and we have a count going, i.e., request finished
       setSeconds(16)
       const timer = setTimeout(() => setOpacity(0), 3000)
       return () => clearTimeout(timer)
     } else if (loading) { // loading is true
-      if (seconds > 15 || seconds == 0) setSeconds(1)
+      if (seconds > 15 || seconds === 0) setSeconds(1)
       setOpacity(1)
-      const timer = setInterval(() => {
+      const interval = setInterval(() => {
         if (seconds < 15) setSeconds(seconds + 1)
       }, 1000)
-      return () => clearInterval(timer)
+      return () => clearInterval(interval)
     }
-  }, [loading, seconds])
+  }, [loading, seconds, opacity])
+
+  useEffect(() => {
+    // the purpose of something like the "visible" state variable might be to unmount the progress bar completely
+    // and immediately remount, helping avoid animating from 100% to 10%
+    if (!opacity) {
+      const timer = setTimeout(() => setVisible(false), 1000)
+      return clearTimeout(timer)
+    }
+  }, [opacity])
 
   const width = seconds <= 15
     ? `${.90 * (seconds / 15 * 100)}%`
     : "100%"
 
   return (
-    <div {...useCss(css)} className='progressBar' style={{ width, opacity }} />
+    <div data-testid="progress-bar" {...useCss(css)} className='progressBar' style={{ width, opacity }} />
   )
 }
 
