2c1f86def303e71c75b14992e6687045169a92a0
Devin Rasmussen <devin.rasmussen@icloud.com>
2021-07-16 03:11:42 +0700

    Introduce progress bar to Solution component

--- src/progress_bar_exercise/ProgressBarExercise.js
+++ src/progress_bar_exercise/ProgressBarExercise.js
@@ -1,3 +1,4 @@
+import { useCss } from "kremling";
 import React from "react";
 import Exercise from "../exercise/Exercise";
 
@@ -18,5 +19,21 @@ export default ProgressBarExercise;
 // ----------------------------------------------------------------------------------
 
 const Solution = () => {
-  return <div>Add solution here</div>;
+  return (
+    <div {...useCss(css)}>
+      <div className='progressBar' />
+      Add solution here
+    </div>
+  )
 };
+
+const css = `
+  & .progressBar {
+    position: fixed;
+    top: 0;
+    left: 0;
+    height: 6px;
+    width: 100%;
+    background: linear-gradient(to right, var(--sunset), var(--red));
+  }
+`
